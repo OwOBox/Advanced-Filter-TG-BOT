af690546bb39cb2173d05f69a124907d52dfd920
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-30 09:44:35 +0700

    Update __init__.py

--- cinderella/__init__.py
+++ cinderella/__init__.py
@@ -70,7 +70,7 @@ if ENV:
     
     API_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
-    DB_URI = os.environ.get('DATABASE_URL')
+    DB_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) 
     DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
